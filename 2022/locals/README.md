9864a308d6dbc6481339e94b48d08bc79e4b5142
Michal Skop <michal.skop@kohovolit.eu>
2022-09-24 09:57:32 +0200

    extractor

--- 2022/locals/README.md
+++ 2022/locals/README.md
@@ -3,4 +3,6 @@
 1. Downloads: downloader.py downloads data from volby.cz/dedicated server
 rsync data from the other server to local backup
 
-2. Extractor:
+2. Extractor: extracts data from downloaded files to results.csv files
+
+3. 
--- 2022/locals/extractor/extractor.py
+++ 2022/locals/extractor/extractor.py
@@ -6,14 +6,22 @@ from os import mkdir
 import pandas as pd
 import xmltodict
 
+test = False
+if test:
+  teststr = '-test'
+else:
+  teststr = ''
+
 # load settings
 path = '/'.join(abspath(getsourcefile(lambda:0)).split("/")[0:-1]) + "/"
 print(path)
-batches = pd.read_csv(path + '../downloads/batches.csv')
+
+batches = pd.read_csv(path + '../downloads/batches' + teststr + '.csv')
+assemblies = pd.read_csv(path + 'assemblies_80.csv')
 
 for n in batches['n']:
   print('batch ' + str(n))
-  fpath = path + '../downloads/batches/' + str(n) + '.xml'
+  fpath = path + '../downloads/batches' + teststr + '/' + str(n) + '.xml'
   with open(fpath, 'rb') as f:
     text = f.read()
     obj = xmltodict.parse(text)
@@ -22,20 +30,24 @@ for n in batches['n']:
         obj['VYSLEDKY_OKRSKY']['OKRSEK'] = [obj['VYSLEDKY_OKRSKY']['OKRSEK']]
       for okrsek in obj['VYSLEDKY_OKRSKY']['OKRSEK']:
         # create directory if not exists
-        file_path = path + 'results/' + okrsek['@KODZASTUP']
-        if not path.exists(file_path):
-          mkdir(file_path)
-        # read results
-        if exists(file_path + '/'  + 'results.csv'):
-          results = pd.read_csv(file_path + '/'  + 'results.csv')
+        code = int(okrsek['@KODZASTUP'])
+        if code in assemblies['KODZASTUP'].values:
+          file_path = path + 'results' + teststr + '/' + okrsek['@KODZASTUP']
+          if not exists(file_path):
+            mkdir(file_path)
+          # read results
+          if exists(file_path + '/'  + 'results.csv'):
+            results = pd.read_csv(file_path + '/'  + 'results.csv')
+          else:
+            results = pd.DataFrame()
+          if type(okrsek['HLASY_OKRSEK']) != list:
+            okrsek['HLASY_OKRSEK'] = [okrsek['HLASY_OKRSEK']]
+          for hlasy in okrsek['HLASY_OKRSEK']:
+            results = pd.concat([results, pd.DataFrame([{'OKRSEK': okrsek['@CIS_OKRSEK'], 'STRANA': hlasy['@POR_STR_HLAS_LIST'], 'HLASY': hlasy['@HLASY'], 'batch': n}])])
+          # save results
+          results.to_csv(file_path + '/'  + 'results.csv', index=False)
+          results.to_csv(file_path + '/'  + 'results_' + str(n).zfill(3) + '.csv', index=False)
         else:
-          results = pd.DataFrame()
-        if type(okrsek['HLASY_OKRSEK']) != list:
-          okrsek['HLASY_OKRSEK'] = [okrsek['HLASY_OKRSEK']]
-        for hlasy in okrsek['HLASY_OKRSEK']:
-          results = pd.concat([results, pd.DataFrame([{'OKRSEK': okrsek['@CIS_OKRSEK'], 'STRANA': hlasy['@POR_STR_HLAS_LIST'], 'HLASY': hlasy['@HLASY'], 'batch': n}])])
-        # save results
-        results.to_csv(file_path + '/'  + 'results.csv', index=False)
-        results.to_csv(file_path + '/'  + 'results_' + str(n).zfill(3) + '.csv', index=False)
+          pass # not in selected assemblies
     else:
       print('no OKRSEK')
