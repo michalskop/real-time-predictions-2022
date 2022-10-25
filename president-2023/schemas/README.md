45cff90658c2bedf516209dae7d1ee0f0256a9c9
Michal Skop <michal.skop@kohovolit.eu>
2022-10-26 00:31:56 +0200

    schemas and examples for presidential elections

---
+++ president-2023/schemas/README.md
@@ -0,0 +1,24 @@
+# Schemas and examples for the 2023 presidential election real-time estimates
+
+## Chart
+- JSON Schema: [results.schema.json](results.schema.json)
+- Example: [results-example-0.json](results-example-0.json)
+
+## Map(s)
+- JSON Schema: [maps.schema.json](maps.schema.json)
+- Simple Example: [map-example-0.json](map-example-0.json)
+- Full Example: [map-example-1.json](map-example-1.json)
+* Note: Districts may not be used in the final version of the map(s).
+
+## Lists
+- Regions (kraje, _NUTS 3_): [regions.csv](regions.csv)
+- Districts (okresy, _LAU 1_): [districts.csv](districts.csv)
+* Note: Districts may not be used in the final version of the map(s).
+
+## Links
+- [JSON Schema](http://json-schema.org/)
+- [JSON Schema Validator](https://www.jsonschemavalidator.net/)
+
+## Versioning
+0.1.0 - Initial version
+
---
+++ president-2023/schemas/utils/generate_example.py
@@ -0,0 +1,35 @@
+"""Generates example data for a given schema."""
+
+import json
+import pandas as pd
+import random
+
+path = "president-2023/schemas/"
+
+out = {
+  "datetime": "2023-01-15T15:35:00",
+  "counted": 47.59,
+  "confidence": 95,
+  "maps": []
+}
+
+maps = [{"level": "NUTS 3", "file": "regions.csv"}, {"level": "LAU 1", "file": "districts.csv"}]
+winners = [{"id": "petr-pavel", "name": "Petr Pavel"}, {"id": "danuse-nerudova", "name": "Danuše Nerudová"}, {"id": "andrej-babis", "name": "Andrej Babiš"}, {"id": None, "name": None}]
+
+for map in maps:
+  df = pd.read_csv(path + map["file"])
+  map = {"level": map["level"], "regions": []}
+  for i, row in df.iterrows():
+    winner = random.choice(range(0, len(winners) - 1))
+    region = {
+      "id": row["id"],
+      "name": row["name"],
+      "counted": random.randint(0, 100),
+      "winner-id": winners[winner]["id"],
+      "winner-name": winners[winner]["name"],
+    }
+    map["regions"].append(region)
+  out["maps"].append(map)
+
+with open(path + "map-example-1.json", "w") as f:
+  json.dump(out, f, indent=2, ensure_ascii=False)
