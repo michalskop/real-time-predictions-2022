161392f4f222392e63048ecd984d8e4dc0017084
Michal Skop <michal.skop@kohovolit.eu>
2023-01-10 12:25:01 +0100

    update 2023-01-10T12:25:01+01:00

--- president-2023/round-1/extract/extractor.py
+++ president-2023/round-1/extract/extractor.py
@@ -9,7 +9,7 @@ import xmltodict
 
 # load settings
 path = '/'.join(abspath(getsourcefile(lambda:0)).split("/")[0:-1]) + "/"
-path = "/home/michal/dev/real-time-predictions-2022/president-2023/round-1/extract/" # ** for testing only **
+# path = "/home/michal/dev/real-time-predictions-2022/president-2023/round-1/extract/" # ** for testing only **
 if exists(path + "../../settings.json"):
   with open(path + '../../settings.json') as f:
     settings = json.load(f)
