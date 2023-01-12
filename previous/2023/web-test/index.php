e21fe4b224d465a07316349dc91d020719acfb1a
Michal Skop <michal.skop@kohovolit.eu>
2023-01-12 22:16:01 +0100

    update 2023-01-12T22:16:01+01:00

--- previous/2023/web-test/index.php
+++ previous/2023/web-test/index.php
@@ -4,25 +4,34 @@
 header('Content-Type: application/xml');
 
 if (isset($_GET['davka']) & $_GET['davka'] != "") {
-  $f = file_get_contents("batches/" . $_GET['davka'] . ".xml");
+  $f = file_get_contents("batches-test-data/" . $_GET['davka'] . ".xml");
   echo($f);
   exit();
 }
 
-$delay = "02:00:00"; // calculate GMT difference
+$delay = "13:20:00"; // calculate GMT difference
 $last = 0;
 
-$batches = array_map('str_getcsv', file('batches.csv'));
+$batches = array_map('str_getcsv', file('batches-test-data.csv'));
 
 for ($i = 1; $i < count($batches); $i++) {
-  // print_r(date("H:i:s", strtotime(substr($batches[$i][1], 11)) - strtotime($delay)));
+  // print_r(date("H:i:s", strtotime(substr($batches[$i][1], 11)) + strtotime($delay)));
   // echo("<br/>");
+  // print_r(date("H:i:s", strtotime(substr($batches[$i][1], 11))));
+  // echo("<br/>");
+  // print_r(strtotime(substr($batches[$i][1], 11)));
+  // echo("<br/>");
+  // print_r(strtotime($delay));
+  // die();
   if((date("H:i:s", strtotime(substr($batches[$i][1], 11)) + strtotime($delay))) < date("H:i:s")) {
     $last = $i;
+    // echo("<br/>");
+    // print($last);
+    // die();
   }
 }
 
-$f = file_get_contents("batches/" . $last . ".xml");
+$f = file_get_contents("batches-test-data/" . $last . ".xml");
 echo($f);
 
 ?>
