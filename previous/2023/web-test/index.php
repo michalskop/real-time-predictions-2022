9dd9c0aec06a0e8707ce8403cd0cd57d31fa906e
Michal Skop <michal.skop@kohovolit.eu>
2023-01-13 00:23:01 +0100

    update 2023-01-13T00:23:01+01:00

--- previous/2023/web-test/index.php
+++ previous/2023/web-test/index.php
@@ -9,24 +9,20 @@ if (isset($_GET['davka']) & $_GET['davka'] != "") {
   exit();
 }
 
-$delay = "14:00:00"; // calculate GMT difference
+$delay = "15:20:00"; // calculate GMT difference
 $last = 0;
 
 $batches = array_map('str_getcsv', file('batches-test-data.csv'));
 
 for ($i = 1; $i < count($batches); $i++) {
   // print_r(date("H:i:s", strtotime(substr($batches[$i][1], 11)) + strtotime($delay)));
-  // echo("<br/>");
-  // print_r(date("H:i:s", strtotime(substr($batches[$i][1], 11))));
-  // echo("<br/>");
-  // print_r(strtotime(substr($batches[$i][1], 11)));
-  // echo("<br/>");
-  // print_r(strtotime($delay));
+  // echo("<br/>\n");
+  // print_r(date("H:i:s"));
+  // echo("<br/>\n\n");
   // die();
   if((date("H:i:s", strtotime(substr($batches[$i][1], 11)) + strtotime($delay))) < date("H:i:s")) {
     $last = $i;
-    // echo("<br/>");
-    // print($last);
+    // print("last: " . $last . "<br/>\n");
     // die();
   }
 }
