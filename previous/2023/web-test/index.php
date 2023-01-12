005826a18e05cefe5bfb8b5cb88ba5a4a5289a4e
Michal Skop <michal.skop@kohovolit.eu>
2023-01-12 23:58:01 +0100

    update 2023-01-12T23:58:01+01:00

--- previous/2023/web-test/index.php
+++ previous/2023/web-test/index.php
@@ -9,20 +9,20 @@ if (isset($_GET['davka']) & $_GET['davka'] != "") {
   exit();
 }
 
-$delay = "13:20:00"; // calculate GMT difference
+$delay = "14:00:00"; // calculate GMT difference
 $last = 0;
 
 $batches = array_map('str_getcsv', file('batches-test-data.csv'));
 
 for ($i = 1; $i < count($batches); $i++) {
-  // print_r(date("H:i:s", strtotime(substr($batches[$i][1], 11)) + strtotime($delay)));
-  // echo("<br/>");
-  // print_r(date("H:i:s", strtotime(substr($batches[$i][1], 11))));
-  // echo("<br/>");
-  // print_r(strtotime(substr($batches[$i][1], 11)));
-  // echo("<br/>");
-  // print_r(strtotime($delay));
-  // die();
+  print_r(date("H:i:s", strtotime(substr($batches[$i][1], 11)) + strtotime($delay)));
+  echo("<br/>");
+  print_r(date("H:i:s", strtotime(substr($batches[$i][1], 11))));
+  echo("<br/>");
+  print_r(strtotime(substr($batches[$i][1], 11)));
+  echo("<br/>");
+  print_r(strtotime($delay));
+  die();
   if((date("H:i:s", strtotime(substr($batches[$i][1], 11)) + strtotime($delay))) < date("H:i:s")) {
     $last = $i;
     // echo("<br/>");
