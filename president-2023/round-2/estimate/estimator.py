e41e9f1f827e6b540be3f0d2f0dc0b9fd9ff572f
Michal Skop <michal.skop@kohovolit.eu>
2023-01-26 18:00:01 +0100

    update 2023-01-26T18:00:01+01:00

--- president-2023/round-2/estimate/estimator.py
+++ president-2023/round-2/estimate/estimator.py
@@ -124,6 +124,8 @@ else:
 hit = gt * (1 + val)
 lot = gt * (1 / (1 + val))
 
+# 2 candidates: we used the lower values (2nd) to estimate the upper values (1st)
+
 # prepare output
 # gains + candidates
 gain = pd.concat([gt, hit, lot], axis=0)
