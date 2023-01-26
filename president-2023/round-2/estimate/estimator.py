ded30cebdb15ea57743045685884f77b53e82610
Michal Skop <michal.skop@kohovolit.eu>
2023-01-26 11:53:02 +0100

    update 2023-01-26T11:53:01+01:00

--- president-2023/round-2/estimate/estimator.py
+++ president-2023/round-2/estimate/estimator.py
@@ -342,7 +342,7 @@ if (counted > 2): # minimal 2% counted
       'winner': r['winner_number'],
       'winner-name': r['name'],
       'winner-id': r['candidate_id'],
-      'counted': r['counted'],
+      'counted': round(r['counted'] * 10) / 10,
     })
   # with open(path + '../../../docs/president-2023/round-1/map-v1' + teststr + '.json', 'w') as outfile:
   with open(path + '../../../docs/president-2023/round-2/map-v1.json', 'w') as outfile:
