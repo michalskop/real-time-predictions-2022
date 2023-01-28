59f609b8b3aa6267e0e0635e867bcd50abc33bb1
Michal Skop <michal.skop@kohovolit.eu>
2023-01-28 15:24:01 +0100

    update 2023-01-28T15:24:01+01:00

--- previous/2023/map_ps.py
+++ previous/2023/map_ps.py
@@ -1,3 +1,8 @@
 """Polling stations for map."""
 
 import pandas as pd
+
+path = "/home/michal/dev/real-time-predictions-2022/previous/2023/"
+
+polling_stations = pd.read_csv(path + "polling_stations.csv")
+
