4231159b92175a7c7eb604cc1f76ea12a7060c6a
Michal Skop <michal.skop@kohovolit.eu>
2023-01-28 15:25:01 +0100

    update 2023-01-28T15:25:01+01:00

--- previous/2023/map_ps.py
+++ previous/2023/map_ps.py
@@ -3,6 +3,8 @@
 import pandas as pd
 
 path = "/home/michal/dev/real-time-predictions-2022/previous/2023/"
+path0 = "/home/michal/dev/real-time-predictions-2022/president-2023/round-2/extract/results/"
 
 polling_stations = pd.read_csv(path + "polling_stations.csv")
 
+results = pd.read_csv(path + "results.csv")
